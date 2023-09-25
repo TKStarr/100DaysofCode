f541587964d1e6742cabd3da80b7b191604f902c
TKStarr <timstarr88@gmail.com>
2023-09-25 12:02:56 -0400

    Day 03 Final Answer

--- Day_03/Day_03.py
+++ Day_03/Day_03.py
@@ -1,1 +1,47 @@
-import numpy as np
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+step1 = input("Do you go left or right\n")
+
+if step1 == "left":
+  step2 = input("You come to a river.  Do you swim or wait?\n")
+  if step2 == "wait":
+    step3 = input("You come to a wall with three doors.  Do you go through the red, blue, or yellow door?\n")
+    if step3 == "yellow":
+      print("You Win!")
+    elif step3 == "red":
+      print("Burned by fire.  Game Over.")
+    elif step3 == "blue":
+      print("Eaten by Beasts.  Game Over.")
+    else:
+      print("Game Over.")
+  else:
+    print("Attacked by Trout.  Game Over.")
+else:
+  print("Fall in a hole.  Game Over.")
