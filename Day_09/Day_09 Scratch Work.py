2ed096467a3751f069b5fbf5a58355d44f257e51
TKStarr <timstarr88@gmail.com>
2023-10-08 13:23:37 -0400

    Day 09 Exercises

---
+++ Day_09/Day_09 Scratch Work.py
@@ -0,0 +1,25 @@
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"],
+               "bucket_list": ["Marseille", "Lyon", "Monaco"],
+               "total_visits": 12,
+               },
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+                "bucket_list": ["Munich, Dusseldorf, Frankfurt"],
+                "total_visits": 5,
+                }
+}
+
+travel_log_2 = [
+    {
+    "country": "France",
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "bucket_list": ["Marseille", "Lyon", "Monaco"],
+    "total_visits": 12,
+    },
+    {
+    "country": "Germany",
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "bucket_list": ["Munich, Dusseldorf, Frankfurt"],
+    "total_visits": 5,
+     }
+]
