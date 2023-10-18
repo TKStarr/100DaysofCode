4b4cc5b3973f2607ed0dc1ba608f57c894c22c7d
TKStarr <timstarr88@gmail.com>
2023-10-18 09:40:19 -0400

    Day 12 Final Answer

--- Day_12/Day_12 Number guessing game.py
+++ Day_12/Day_12 Number guessing game.py
@@ -4,31 +4,36 @@ secret_number = rd.randint(1,100)
 
 difficulty = input("Do you want to play on easy or hard mode?")
 
-guess_list = []
+guesses_list = []
 
 if difficulty == 'easy':
-    num_guesses = 9
+    num_guesses = 10
 elif difficulty == "hard":
-    num_guesses = 4
+    num_guesses = 5
 else:
     print('Please type either easy or hard.')
 
 guess = int(input("Please guess a number"))
-guess_list.append(guess)
+guesses_list.append(guess)
 
 while num_guesses > 0 and guess != secret_number:
     if guess > secret_number:
-        print(f"Too high.\n You have {num_guesses} guesses remaining.")
+        print(f"Too high.\n You have {num_guesses - 1} guesses remaining.")
         num_guesses = num_guesses - 1
-        guess = int(input("Make a guess."))
+        if num_guesses > 0:
+            guess = int(input("Make a guess."))
+            guesses_list.append(guess)
     elif guess < secret_number:
-        print(f"Too low.\n You have {num_guesses} guesses remaining.")
+        print(f"Too low.\n You have {num_guesses - 1} guesses remaining.")
         num_guesses = num_guesses - 1
-        guess = int(input("Make a guess."))
+        if num_guesses > 0:
+            guess = int(input("Make a guess."))
+            guesses_list.append(guess)
     else:
         print('You guessed correctly!')
+        guesses_list.append(guess)
 
-    guess_list.append(guess)
+
 
 if guess == secret_number:
     print(f"You got it!  The answer was {secret_number}.")
