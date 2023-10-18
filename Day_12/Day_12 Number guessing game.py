b3ab7bdc4f5a0101540573abcd26f0c7b5f79daa
TKStarr <timstarr88@gmail.com>
2023-10-18 10:01:49 -0400

    Day 12 Final Answer

--- Day_12/Day_12 Number guessing game.py
+++ Day_12/Day_12 Number guessing game.py
@@ -13,25 +13,21 @@ elif difficulty == "hard":
 else:
     print('Please type either easy or hard.')
 
-guess = int(input("Please guess a number"))
-guesses_list.append(guess)
+
+guess = 0
 
 while num_guesses > 0 and guess != secret_number:
+    guess = int(input("Please guess a number"))
+    guesses_list.append(guess)
     if guess > secret_number:
         print(f"Too high.\n You have {num_guesses - 1} guesses remaining.")
         num_guesses = num_guesses - 1
-        if num_guesses > 0:
-            guess = int(input("Make a guess."))
-            guesses_list.append(guess)
     elif guess < secret_number:
         print(f"Too low.\n You have {num_guesses - 1} guesses remaining.")
         num_guesses = num_guesses - 1
-        if num_guesses > 0:
-            guess = int(input("Make a guess."))
-            guesses_list.append(guess)
     else:
         print('You guessed correctly!')
-        guesses_list.append(guess)
+
 
 
 
